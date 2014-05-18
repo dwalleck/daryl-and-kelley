ebdae2e2cdb8efb3d9e20951e383c9b0d61dcfc7
Daryl Walleck <daryl.walleck@rackspace.com>
2014-05-18 14:05:41 -0500

    Created barebones template

--- wedding/views.py
+++ wedding/views.py
@@ -1,7 +1,12 @@
-from flask import render_template, flash, redirect, request, url_for
+from flask import render_template
 from wedding import app
 
 
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+
+@app.route('/template')
+def index_template():
+    return render_template("index_template.html")
